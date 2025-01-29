33b919e11cd7774cd5387dcc772f7dbbf21d37b7
Vish <184423+vishvish@users.noreply.github.com>
2025-01-29 15:13:23 +0100

    fix: Update Mac optimizer dialog
    - Replace deprecated Mac-specific modal dialog code
    - Use modern wxWidgets event loop and modal dialog APIs
    - Update copyright symbol format

--- Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_mac.cpp
+++ Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_mac.cpp
@@ -6,9 +6,9 @@ Purpose:      Source file for class tmwxOptimizerDialog, specialization for Mac
 Author:       Stefan Csomor
 Modified by:  Robert J. Lang
 Created:     (original), 2004-04-22 (modifications)
-Copyright (original):       ©2004 by Stefan Csomor, 
+Copyright (original):       2004 by Stefan Csomor, 
                               released under the wxWidgets license
-Copyright (modifications):  ©2004 Robert J. Lang. All Rights Reserved.
+Copyright (modifications):  2004 Robert J. Lang. All Rights Reserved.
 *******************************************************************************/
 
 #ifdef __WXMAC__
@@ -30,6 +30,98 @@ static bool s_formerModal;
 #endif
 
 extern wxWindowList wxModalDialogs;
+
+
+/*****
+Constructor
+*****/
+tmwxOptimizerDialog::tmwxOptimizerDialog(tmwxDesignFrame* parent) : 
+  wxDialog(parent, wxID_ANY, wxT("Optimizing..."), wxDefaultPosition, 
+    wxDefaultSize, wxCAPTION | wxSYSTEM_MENU)
+{
+  // Make this dialog modal
+  MakeModal(true);
+
+  // Create the dialog box contents
+  wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
+  SetSizer(topSizer);
+  
+  // Add a gauge for showing progress
+  mGauge = new wxGauge(this, wxID_ANY, 100);
+  topSizer->Add(mGauge, 0, wxALL | wxEXPAND, 10);
+  
+  // Add a text field for showing current status
+  mText = new wxStaticText(this, wxID_ANY, wxT("Initializing..."));
+  topSizer->Add(mText, 0, wxALL | wxEXPAND, 10);
+  
+  // Add a button for stopping the optimization
+  mStopButton = new wxButton(this, wxID_ANY, wxT("Stop"));
+  topSizer->Add(mStopButton, 0, wxALL | wxALIGN_CENTER_HORIZONTAL, 10);
+  
+  // Set up event handlers
+  mStopButton->Bind(wxEVT_BUTTON, &tmwxOptimizerDialog::OnStop, this);
+  
+  // Size the dialog box to fit its contents
+  topSizer->Fit(this);
+  topSizer->SetSizeHints(this);
+  
+  // Center the dialog box on the screen
+  Center();
+}
+
+
+/*****
+Event table
+*****/
+void tmwxOptimizerDialog::OnStop(wxCommandEvent&)
+{
+  mStopPressed = true;
+}
+
+
+/*****
+Destructor
+*****/
+tmwxOptimizerDialog::~tmwxOptimizerDialog()
+{
+  MakeModal(false);
+}
+
+
+/*****
+Process one event
+*****/
+void tmwxOptimizerDialog::ProcessEvent()
+{
+  wxEventLoopBase::GetActive()->Dispatch();
+}
+
+
+/*****
+Set the gauge value
+*****/
+void tmwxOptimizerDialog::SetGaugeValue(int value)
+{
+  mGauge->SetValue(value);
+}
+
+
+/*****
+Set the text value
+*****/
+void tmwxOptimizerDialog::SetText(const wxString& text)
+{
+  mText->SetLabel(text);
+}
+
+
+/*****
+Return true if the stop button was pressed
+*****/
+bool tmwxOptimizerDialog::StopPressed()
+{
+  return mStopPressed;
+}
 
 
 /*****

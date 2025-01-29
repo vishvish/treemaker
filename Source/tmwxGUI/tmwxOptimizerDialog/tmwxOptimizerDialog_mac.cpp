d4ca1ffe5ada070aca0f7b435eccbded9eda04eb
Vish <184423+vishvish@users.noreply.github.com>
2025-01-29 15:17:19 +0100

    fix: Update Mac optimizer dialog implementation
    - Replace deprecated Mac-specific modal dialog code with modern wxWidgets APIs
    - Fix event loop handling using wxEventLoopBase
    - Update header includes for event loop support

--- Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_mac.cpp
+++ Source/tmwxGUI/tmwxOptimizerDialog/tmwxOptimizerDialog_mac.cpp
@@ -13,53 +13,39 @@ Copyright (modifications):  2004 Robert J. Lang. All Rights Reserved.
 
 #ifdef __WXMAC__
 
-// Platform-specific includes
-#include <Carbon/Carbon.h>
-
 #include "tmwxOptimizerDialog.h"
+#include "tmwxDoc.h"
+#include "tmwxDesignFrame.h"
+#include "tmOptimizer.h"
+#include "tmwxStr.h"
 
+#include "wx/app.h"
+#include "wx/evtloop.h"
 
 /**********
 class tmwxOptimizerDialog
-Dialog for long, cancellable calculations
+A dialog for long, cancellable calculations
 **********/
-
-#if !TARGET_CARBON
-extern bool s_macIsInModalLoop;
-static bool s_formerModal;
-#endif
-
-extern wxWindowList wxModalDialogs;
-
 
 /*****
 Constructor
 *****/
-tmwxOptimizerDialog::tmwxOptimizerDialog(tmwxDesignFrame* parent) : 
-  wxDialog(parent, wxID_ANY, wxT("Optimizing..."), wxDefaultPosition, 
-    wxDefaultSize, wxCAPTION | wxSYSTEM_MENU)
+tmwxOptimizerDialog::tmwxOptimizerDialog(tmwxDoc* aDoc, tmOptimizer* aOptimizer, 
+  const wxString& descr) : 
+  wxDialog(NULL, wxID_ANY, wxT("Optimizing..."), wxDefaultPosition, 
+    wxDefaultSize, wxCAPTION | wxSYSTEM_MENU),
+  mDoc(aDoc),
+  mOptimizer(aOptimizer),
+  mStatus(IN_LOOP),
+  mReason(0)
 {
-  // Make this dialog modal
-  MakeModal(true);
-
   // Create the dialog box contents
   wxBoxSizer* topSizer = new wxBoxSizer(wxVERTICAL);
   SetSizer(topSizer);
   
-  // Add a gauge for showing progress
-  mGauge = new wxGauge(this, wxID_ANY, 100);
-  topSizer->Add(mGauge, 0, wxALL | wxEXPAND, 10);
-  
-  // Add a text field for showing current status
-  mText = new wxStaticText(this, wxID_ANY, wxT("Initializing..."));
-  topSizer->Add(mText, 0, wxALL | wxEXPAND, 10);
-  
-  // Add a button for stopping the optimization
-  mStopButton = new wxButton(this, wxID_ANY, wxT("Stop"));
-  topSizer->Add(mStopButton, 0, wxALL | wxALIGN_CENTER_HORIZONTAL, 10);
-  
-  // Set up event handlers
-  mStopButton->Bind(wxEVT_BUTTON, &tmwxOptimizerDialog::OnStop, this);
+  // Add text for showing progress
+  mProgress = new tmwxStaticText(this, wxID_ANY, descr);
+  topSizer->Add(mProgress, 0, wxALL | wxEXPAND, 10);
   
   // Size the dialog box to fit its contents
   topSizer->Fit(this);
@@ -73,108 +59,108 @@ tmwxOptimizerDialog::tmwxOptimizerDialog(tmwxDesignFrame* parent) :
 /*****
 Event table
 *****/
-void tmwxOptimizerDialog::OnStop(wxCommandEvent&)
+BEGIN_EVENT_TABLE(tmwxOptimizerDialog, wxDialog)
+  EVT_KEY_DOWN(tmwxOptimizerDialog::OnKeyDown)
+END_EVENT_TABLE()
+
+
+/*****
+Event handlers
+*****/
+void tmwxOptimizerDialog::OnKeyDown(wxKeyEvent& event)
 {
-  mStopPressed = true;
+  if (event.GetKeyCode() == WXK_ESCAPE) {
+    SetStatus(USER_CANCELLED);
+    SetReason(0);
+  }
+  event.Skip();
 }
 
 
 /*****
-Destructor
+Set the status
 *****/
-tmwxOptimizerDialog::~tmwxOptimizerDialog()
+void tmwxOptimizerDialog::SetStatus(int aStatus)
 {
-  MakeModal(false);
+  mStatus = aStatus;
 }
 
 
 /*****
-Process one event
+Set the reason
 *****/
-void tmwxOptimizerDialog::ProcessEvent()
+void tmwxOptimizerDialog::SetReason(int aReason)
+{
+  mReason = aReason;
+}
+
+
+/*****
+Start the modal dialog
+*****/
+void tmwxOptimizerDialog::DoStartModal()
+{
+  Show();
+}
+
+
+/*****
+Run the event loop for the modal dialog
+*****/
+void tmwxOptimizerDialog::DoEventLoopModal()
+{
+  while (mStatus == IN_LOOP) {
+    DoEventLoopOnce();
+  }
+}
+
+
+/*****
+Process one event in the modal dialog
+*****/
+void tmwxOptimizerDialog::DoEventLoopOnce()
 {
   wxEventLoopBase::GetActive()->Dispatch();
 }
 
 
 /*****
-Set the gauge value
-*****/
-void tmwxOptimizerDialog::SetGaugeValue(int value)
-{
-  mGauge->SetValue(value);
-}
-
-
-/*****
-Set the text value
-*****/
-void tmwxOptimizerDialog::SetText(const wxString& text)
-{
-  mText->SetLabel(text);
-}
-
-
-/*****
-Return true if the stop button was pressed
-*****/
-bool tmwxOptimizerDialog::StopPressed()
-{
-  return mStopPressed;
-}
-
-
-/*****
-Put up and show the modal dialog. This code is basically everything that happens
-in a call to wxDialog::ShowModal(), up to the start of the dialog event loop.
-*****/
-void tmwxOptimizerDialog::DoStartModal()
-{
-  ::wxBeginBusyCursor();
-  SetModal(true);
-  if (!wxDialogBase::Show(true)) 
-    TMFAIL("error, dialog was already visible in "\
-      "tmwxOptimizerDialog::DoStartModal()");
-  InitDialog();
-  wxModalDialogs.Append(this);
-#if TARGET_CARBON
-  BeginAppModalStateForWindow((WindowRef) MacGetWindowRef());
-#else
-  s_formerModal = s_macIsInModalLoop;
-  s_macIsInModalLoop = true;
-#endif
-}
-
-
-/*****
-Process a single event. Subclasses that implement their own event loop must
-call this routine periodically through the loop.
-*****/
-void tmwxOptimizerDialog::DoEventLoopOnce()
-{
-  wxTheApp->MacDoOneEvent();
-}
-
-
-/*****
-Take down the modal dialog and do cleanup. This code is basically 
-wxDialog::Show(false), followed by everything
-that happens in a call to wxDialog::ShowModal() after the event loop.
+Clean up after the modal dialog
 *****/
 void tmwxOptimizerDialog::DoFinishModal()
 {
-  if (!wxDialogBase::Show(false))
-    TMFAIL("error, dialog was already hidden in "\
-      "tmwxOptimizerDialog::DoFinishModal()");
-  wxModalDialogs.DeleteObject(this);
-  
-#if TARGET_CARBON
-  EndAppModalStateForWindow((WindowRef) MacGetWindowRef());
-#else
-  s_macIsInModalLoop = s_formerModal;
-#endif
-  
-  ::wxEndBusyCursor();
+  Hide();
+}
+
+
+/*****
+End the modal dialog
+*****/
+void tmwxOptimizerDialog::EndModal(int retCode)
+{
+  SetStatus(retCode);
+}
+
+
+/*****
+Show the modal dialog
+*****/
+int tmwxOptimizerDialog::ShowModal()
+{
+  DoStartModal();
+  DoEventLoopModal();
+  DoFinishModal();
+  return GetStatus();
+}
+
+
+/*****
+Update the UI
+*****/
+void tmwxOptimizerDialog::UpdateUI()
+{
+  mProgress->SetLabel(mOptimizer->GetStatusStr());
+  DoEventLoopOnce();
 }
 
 #endif // __WXMAC__
